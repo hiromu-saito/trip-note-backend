21984a99cb186074d335fed2230f8162f0c2c53f
hiromu_saito <hiromu.saito@bigtreetc.com>
2022-01-15 09:15:15 +0000

    add memory table

--- mysql/docker-entrypoint-initdb.d/initdb.sql
+++ mysql/docker-entrypoint-initdb.d/initdb.sql
@@ -1,5 +1,15 @@
-CREATE TABLE users (
-  id        bigint(20)     PRIMARY KEY,
-  email     varchar(200)   DEFAULT NULL UNIQUE,
-  password longblob
+CREATE TABLE IF NOT EXISTS users (
+  id        bigint(20)     PRIMARY KEY          comment 'ユーザーID'
+ ,email     varchar(200)   DEFAULT NULL UNIQUE  comment 'メールアドレス'
+ ,password  longblob                             comment 'パスワード'
 );
+
+create table if not exists `trip-note`.`memories` (
+    id int primary key comment '思い出ID'
+  , user_id int not null comment 'ユーザID'
+  , hotel_name varchar(30) not null comment 'ホテル名称'
+  , hotel_image varchar(100) not null comment 'ホテル写真'
+  , impression varchar(30) comment '感想'
+  , accommodation_date date not null comment '宿泊日'
+  , delete_flag int default 0 not null comment '削除フラグ:0:有効,1:無効'
+)
