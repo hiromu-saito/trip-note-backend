6b156ea8b07606c37bf5c037e50995119786cb5d
hiromu_saito <hiromu.saito@bigtreetc.com>
2022-03-10 22:18:04 +0900

    fix:dbのホストを環境変数で指定するよう修正

--- api/database/database.go
+++ api/database/database.go
@@ -10,17 +10,19 @@ import (
 )
 
 var (
-	schema = "%s:%s@tcp(mysql:3306)/%s?charset=utf8&parseTime=True&loc=Local"
+	schema = "%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local"
 	// docker-compose.ymlに設定した環境変数を取得
 	username       = os.Getenv("MYSQL_USER")
 	password       = os.Getenv("MYSQL_PASSWORD")
 	dbName         = os.Getenv("MYSQL_DATABASE")
-	datasourceName = fmt.Sprintf(schema, username, password, dbName)
+	dbHost         = os.Getenv("MYSQL_HOST")
+	datasourceName = fmt.Sprintf(schema, username, password, dbHost, dbName)
 	Db             *sqlx.DB
 )
 
 func init() {
 	log.Println("database setup")
+	log.Println("dsn:", datasourceName)
 	connection, err := sqlx.Open("mysql", datasourceName)
 
 	if err != nil {
